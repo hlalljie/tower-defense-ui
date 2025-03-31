90ad44075c62cbc760b9e5f9bf7479046bfaf111
hlalljie <hlalljie@gmail.com>
2025-03-31 10:39:19 -0700

    Add simple player controller

---
+++ scenes/player_camera.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+
+@export var pan_speed = 500.0  # Camera movement speed
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input_direction = Vector2.ZERO
+	
+	# Handle keyboard input for panning
+	if Input.is_action_pressed("ui_right"):
+		input_direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		input_direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		input_direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		input_direction.y -= 1
+	
+	# Apply the movement
+	position += input_direction * pan_speed * delta
